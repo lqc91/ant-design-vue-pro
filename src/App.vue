cabf14daa71f41a0fe71e1a71fb663eae0e6b355
lufan <ericlufan@163.com>
2019-11-19 16:37:27 +0800

    设计高扩展性路由; 使用 NProgress 进度条

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <a-button>按钮</a-button>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link> 默认渲染为 <a> 标签，但更好用 -->
+      <router-link to="/dashboard/analysis">dashboard</router-link>|
+      <router-link to="/form">form</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,123 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import NotFound from "../views/404";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: "/user",
+    component: () =>
+      import(/* webpackChunkName: "layout" */ "../layouts/UserLayout"),
+    children: [
+      {
+        path: "/user",
+        redirect: "/user/login"
+      },
+      {
+        path: "/user/login",
+        name: "login",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../views/User/Login")
+      },
+      {
+        path: "/user/register",
+        name: "register",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../views/User/Register")
+      }
+    ]
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/",
+    meta: { authority: ["user", "admin"] },
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout"),
+    children: [
+      // dashboard
+      {
+        path: "/",
+        redirect: "/dashboard/analysis"
+      },
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        meta: { icon: "dashboard", title: "仪表盘" },
+        component: { render: h => h("router-view") },
+        children: [
+          {
+            path: "/dashboard/analysis",
+            name: "analysis",
+            meta: { title: "分析页" },
+            component: () =>
+              import(
+                /* webpackChunkName: "dashboard" */ "../views/Dashboard/Analysis"
+              )
+          }
+        ]
+      },
+      // form
+      {
+        path: "/form",
+        name: "form",
+        component: { render: h => h("router-view") },
+        meta: { icon: "form", title: "表单", authority: ["admin"] },
+        children: [
+          {
+            path: "/form/basic-form",
+            name: "basicform",
+            meta: { title: "基础表单" },
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/BasicForm")
+          },
+          {
+            path: "/form/step-form",
+            name: "stepform",
+            hideChildrenInMenu: true,
+            meta: { title: "分布表单" },
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/StepForm"),
+            children: [
+              {
+                path: "/form/step-form",
+                redirect: "/form/step-form/info"
+              },
+              {
+                path: "/form/step-form/info",
+                name: "info",
+                component: () =>
+                  import(
+                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step1"
+                  )
+              },
+              {
+                path: "/form/step-form/confirm",
+                name: "confirm",
+                component: () =>
+                  import(
+                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step2"
+                  )
+              },
+              {
+                path: "/form/step-form/result",
+                name: "result",
+                component: () =>
+                  import(
+                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step3"
+                  )
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: "*",
+    name: "404",
+    component: NotFound
   }
 ];
 
@@ -25,6 +125,17 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+// 全局后置钩子
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
